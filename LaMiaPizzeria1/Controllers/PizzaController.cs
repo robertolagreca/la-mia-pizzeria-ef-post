a777e3e629ac1dc0fab2937553fb4fcd3e03fc28
robertolagreca <robertolagreca92@gmail.com>
2023-01-10 14:37:43 +0100

    Aggiungere i file di progetto.

---
+++ LaMiaPizzeria1/Controllers/PizzaController.cs
@@ -0,0 +1,32 @@
+﻿using LaMiaPizzeriaModel.Models;
+using LaMiaPizzeriaModel.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace LaMiaPizzeriaModel.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> listPizzas = PizzaData.GetPizzas();
+            return View("Index" , listPizzas);
+        }
+
+        public IActionResult Details(int id)
+        {
+            List<Pizza> listPizzas = PizzaData.GetPizzas();
+
+            foreach (Pizza pizza in listPizzas)
+            {
+                if (pizza.Id == id)
+                {
+                    return View(pizza);
+                }
+            }
+
+            return NotFound("La pizza non esiste!");
+        }
+
+    }
+}
---
+++ LaMiaPizzeria1/Utils/PizzaData.cs
@@ -0,0 +1,34 @@
+﻿using LaMiaPizzeriaModel.Models;
+
+namespace LaMiaPizzeriaModel.Utils
+{
+    //Classe statica che rappresenta la lista di pizze
+    //La lista di pizze sono oggetti di tipo pizza della classe Pizza
+    public static class PizzaData
+    {
+        private static List<Pizza> pizzas;
+
+        public static List<Pizza> GetPizzas()
+        {
+            if (pizzas != null)
+            {
+                return pizzas;
+            }
+
+             pizzas = new List<Pizza>();
+            //su HTML va bene la tilde, su CS e CSS invece solo lo slash e lo legge.
+              Pizza pizza1 = new Pizza(1, "/img/fernando-andrade-_P76trHTWDE-unsplash.jpg", "Margherita", "Sugo, mozzarella", 4.99f);
+              Pizza pizza2 = new Pizza(2, "/img/fernando-andrade-_P76trHTWDE-unsplash.jpg", "Wusterl", "Sugo, mozzarella, wusterl", 5.99f);
+              Pizza pizza3 = new Pizza(3, "/img/fernando-andrade-_P76trHTWDE-unsplash.jpg", "Americana", "Sugo, mozzarella, wusterl, patatine", 6.99f);
+              Pizza pizza4 = new Pizza(4, "/img/fernando-andrade-_P76trHTWDE-unsplash.jpg", "Salamino", "Sugo, mozzarella, salamino piccante", 5.99f); 
+
+              pizzas.Add(pizza1);
+              pizzas.Add(pizza2);
+              pizzas.Add(pizza3);
+              pizzas.Add(pizza4);
+            
+            return pizzas; 
+
+        }
+    }
+}
