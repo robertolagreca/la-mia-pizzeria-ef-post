93328e81d9a14a69e091166e04663114ff36621c
robertolagreca <robertolagreca92@gmail.com>
2023-01-10 14:46:06 +0100

    Aggiunta classe PizzaContext

---
+++ LaMiaPizzeria1/Database/PizzaContext.cs
@@ -0,0 +1,17 @@
+﻿using LaMiaPizzeriaModel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeriaModel.Database
+{
+    public class PizzaContext : DbContext
+    {
+
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=PizzaDBv1;" +
+            "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
