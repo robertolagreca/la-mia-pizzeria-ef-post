731935952cf05c55081c5b7f6c3d43fc618aaae7
robertolagreca <robertolagreca92@gmail.com>
2023-01-12 15:58:34 +0100

    Funzione DELETE completate e funzionante.

--- LaMiaPizzeria1/Controllers/PizzaController.cs
+++ LaMiaPizzeria1/Controllers/PizzaController.cs
@@ -116,5 +116,28 @@ namespace LaMiaPizzeriaModel.Controllers
 
         }
 
+
+        //METODO DELETE (Post)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete (int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza pizzaToDelete = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if(pizzaToDelete != null)
+                {
+                    db.Pizzas.Remove(pizzaToDelete);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                } else
+                {
+                    return NotFound("La pizza da eliminare non è stata trovata");
+                }
+            }
+        }
+
     }
 }
