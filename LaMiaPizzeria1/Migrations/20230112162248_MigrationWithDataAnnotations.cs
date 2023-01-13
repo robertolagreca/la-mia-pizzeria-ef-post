c8c4f046979bb3bdf490d5fcb41da0b359936ac7
robertolagreca <robertolagreca92@gmail.com>
2023-01-13 10:07:26 +0100

    Fix grafico dei bottoni in HTML.

---
+++ LaMiaPizzeria1/Migrations/20230112162248_MigrationWithDataAnnotations.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaMiaPizzeriaModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationWithDataAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Pizzas",
+                type: "float(2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Pizzas",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "float(2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300);
+        }
+    }
+}
